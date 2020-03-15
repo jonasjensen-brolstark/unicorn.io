a4de766d71ea9a894a800607f72318f4d2f5caa6
Jonas Jensen <joens@mhivestasoffshore.com>
2020-03-15 21:03:38 +0100

    image service done

---
+++ src/services/image/Controllers/ImageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace image.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class ImageController : ControllerBase
+  {
+    private readonly IWebHostEnvironment _env;
+
+    private readonly ILogger<ImageController> _logger;
+
+    public ImageController(ILogger<ImageController> logger, IWebHostEnvironment env)
+    {
+      _logger = logger;
+      _env = env;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(Guid id)
+    {
+      var image = GetPhysicalPathFromRelativeUrl($"/Images/{id}.jpg");
+      
+      if (!System.IO.File.Exists(image))
+      {
+        return NotFound();
+      };
+
+      return PhysicalFile(image, "image/jpeg");
+    }
+
+    private string GetPhysicalPathFromRelativeUrl(string url)
+    {
+      var path = Path.Combine(_env.ContentRootPath, url.TrimStart('/'));
+      return path;
+    }
+  }
+}
